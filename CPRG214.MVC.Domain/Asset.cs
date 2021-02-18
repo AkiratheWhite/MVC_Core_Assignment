5653aec17f68cd9e0e2662ac4ca1e813aa7d5d45
OujiKarasuma <li.501stClone@gmail.com>
2021-02-18 13:13:11 -0700

    Final commit. Added additional CSS styling on navbar and added logo/background images.

--- CPRG214.MVC.Domain/Asset.cs
+++ CPRG214.MVC.Domain/Asset.cs
@@ -11,14 +11,14 @@ namespace CPRG214.MVC.Domain
         public int Id { get; set; }
         [Required]
         public int AssetTypeId { get; set; }
-        [Required(ErrorMessage = "Tag Number is required.")]
+        [Required(ErrorMessage = "Tag Number is required.")][Display(Name = "Tag Number")]
         public string TagNumber { get; set; }
         [Required(ErrorMessage = "Manufacturer is required.")]
         public string Manufacturer { get; set; }
         public string Model { get; set; }
         [Required(ErrorMessage = "An asset description is required.")]
         public string Description { get; set; }
-        [Required(ErrorMessage = "The asset's Serial Number is required.")]
+        [Required(ErrorMessage = "The asset's Serial Number is required.")][Display(Name ="Serial Number")]
         public string SerialNumber { get; set; }
         
         //Navigation properties
